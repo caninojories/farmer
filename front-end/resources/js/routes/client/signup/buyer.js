f0273bc7a48dfb08f782a87443e161e9a945cd74
rklumanlan <rklumanlan@gmail.com>
2015-05-18 17:27:49 +0800

    change ; of buyer.js

--- front-end/resources/js/routes/client/signup/buyer.js
+++ front-end/resources/js/routes/client/signup/buyer.js
@@ -19,25 +19,25 @@
       function register_buyer(){
         $q.all([total_searchCallback()])
         .then(function(response){
-          console.log(response)
+          console.log(response);
         }) ;
       }
       function total_searchCallback() {
         return commonsDataService
           .httpPOSTQueryParams('signup/buyer', {
-            first_name : vm.first_name ;
-            last_name : vm.last_name ;
-            email : vm.email ;
-            password : vm.password ;
-            company_name : vm.company_name ;
-            address : vm.address ;
-            city : vm.city ;
-            state : vm.state ;
-            zip : vm.zip ;
-            phone : vm.phone ;
-            desc : vm.desc ;
-            monthly_expense : vm.monthly_expense ;
-            business_type : vm.business_type ;
+            first_name : vm.first_name,
+            last_name : vm.last_name,
+            email : vm.email,
+            password : vm.password,
+            company_name : vm.company_name,
+            address : vm.address,
+            city : vm.city,
+            state : vm.state,
+            zip : vm.zip,
+            phone : vm.phone,
+            desc : vm.desc,
+            monthly_expense : vm.monthly_expense,
+            business_type : vm.business_type,
           }, signupServiceApi)
           .then(function(response) {
             return response;
