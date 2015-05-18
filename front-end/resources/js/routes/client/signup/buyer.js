f6bf021df119b553cf8b99ec2823ff66cb5785f4
JohnMagtoto <JohnMagtoto@gmail.com>
2015-05-18 12:28:07 +0800

    Added submit for form

--- front-end/resources/js/routes/client/signup/buyer.js
+++ front-end/resources/js/routes/client/signup/buyer.js
@@ -11,5 +11,23 @@
     function Buyer($rootScope, commonsDataService) {
       var vm = this;
 
+      // vm.firstName ;
+      // vm.lastName ;
+      // vm.email ;
+      // vm.password ;
+      // vm.companyName ;
+      // vm.address ;
+      // vm.city ;
+      // vm.state ;
+      // vm.zip ;
+      // vm.phoneNum ;
+      // vm.desc ;
+      // vm.monthlyExpense ;
+      // vm.businessType ;
+
+      function registerCustomer(customer){
+
+      }
+
     }
 }());
