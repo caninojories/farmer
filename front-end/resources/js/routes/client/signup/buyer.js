51f242d39beb5796217f00bfc2b8a9be5245c3d8
JohnMagtoto <JohnMagtoto@gmail.com>
2015-05-18 16:16:12 +0800

    Updated buyer.html and buyer.js

--- front-end/resources/js/routes/client/signup/buyer.js
+++ front-end/resources/js/routes/client/signup/buyer.js
@@ -5,10 +5,10 @@
     .module('app.signup')
     .controller('Buyer', Buyer);
 
-    Buyer.$inject = ['$rootScope', 'commonsDataService'];
+    Buyer.$inject = ['$rootScope', 'commonsDataService', '$q', 'signupServiceApi'];
 
     /* @ngInject */
-    function Buyer($rootScope, commonsDataService) {
+    function Buyer($rootScope, commonsDataService, $q, signupServiceApi) {
       var vm = this;
 
       /*literals*/
@@ -30,9 +30,24 @@
       // vm.monthlyExpense ;
       // vm.businessType ;
 
+      // function register_buyer(){
+      //   console.log('buyer')
+      // }
+
       function register_buyer(){
-        console.log('buyer')
+        $q.all([total_searchCallback()])
+        .then(function(response){
+          $rootScope.totalObj = response[0].data.total;
+        }) ;
       }
+      function total_searchCallback() {
+        return commonsDataService
+          .httpPOSTQueryParams('signup/buyer', {}, signupServiceApi)
+          .then(function(response) {
+            return response;
+          });
+      }
+
 
     }
 }());
