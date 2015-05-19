670ec138d1dbac6ebea30eb0a89bd7accbae138f
JohnMagtoto <JohnMagtoto@gmail.com>
2015-05-19 14:41:13 +0800

    Woking on Index page

--- front-end/resources/js/routes/client/main/main.js
+++ front-end/resources/js/routes/client/main/main.js
@@ -5,11 +5,33 @@
     .module('app.main')
     .controller('Main', Main);
 
-    Main.$inject = ['$rootScope'];
+    Main.$inject = ['$rootScope', 'commonsDataService'/*, '$q'*/];
 
     /* @ngInject */
-    function Main($rootScope) {
+    function Main($rootScope, commonsDataService ) {/*commonsDataService, $q*/
       var vm = this;
 
+
+      // function login(){
+      //   $q.all([login_Callback()])
+      //   .then(function(response){
+      //     // console.log(response)
+      //   }) ;
+      // }
+      // function login_Callback() {
+      //   return commonsDataService
+      //     .httpPOSTQueryParams('signup/buyer', {
+      //
+      //     }, /* Service here*/)
+      //     .then(function(response) {
+      //       return response;
+      //     });
+      // }
+      function login(){
+        console.log("login called")
+      }
+      function register(){
+        console.log("register")
+      }
     }
 }());
