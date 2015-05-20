dc6dcc6facf9b0db522a7f7b790c2a78bc9d1e2c
rklumanlan <rklumanlan@gmail.com>
2015-05-20 11:31:23 +0800

    added payment field in account setting

--- back-end/html_routes/client/farmer.js
+++ back-end/html_routes/client/farmer.js
@@ -7,5 +7,9 @@
     res.render('client/farmer/inventory.html');
   });
 
+  router.get('/client/farmer/account_settings.html', function(req, res) {
+    res.render('client/farmer/account_settings.html');
+  });
+
   module.exports = router;
 }());
