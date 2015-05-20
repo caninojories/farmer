07de652627b33818fc45a876a22252cf0bc506b9
rklumanlan <rklumanlan@gmail.com>
2015-05-20 14:19:29 +0800

    acount seting page done, added add inventory modal

--- front-end/resources/js/routes/client/farmer/inventory.js
+++ front-end/resources/js/routes/client/farmer/inventory.js
@@ -5,15 +5,20 @@
   .module('app.farmer')
   .controller('Inventory', Inventory);
 
-  Inventory.$inject = ['logger', 'viewContentLoaded'];
+  Inventory.$inject = ['logger', 'viewContentLoaded','strapModal'];
 
   /* @ngInject */
-  function Inventory(logger, viewContentLoaded) {
+  function Inventory(logger, viewContentLoaded,strapModal) {
     var vm = this;
     vm.add_inventory=add_inventory;
+    vm.add_item=add_item;
 
     function add_inventory(){
       console.log('click');
+      strapModal.show('am-fade-and-scale', 'center', 'commons/add_inventory.html');
+    }
+    function add_item(){
+      console.log('clickssss');
     }
 
   }
